9988dbaa2b49971040e351a89d25d67717bc3968
pan wu_MAC <wupan0577@gmail.com>
2018-04-11 15:58:55 -0400

    project init

---
+++ src/main/java/com/wp/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.wp.dao;
+
+import com.wp.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
